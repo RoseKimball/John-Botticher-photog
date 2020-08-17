0ca3fde101116e3856aa677cb0baa64eb12689aa
RoseKimball <mrkimbal@usc.edu>
2020-08-16 19:28:34 -0700

    Parallax troubleshoot

--- Parallax.js
+++ Parallax.js
@@ -1,14 +1,14 @@
-// let bg = document.getElementById("bg");
-// let moon = document.getElementById("moon");
-// let mountain = document.getElementById("mountain");
-// let road = document.getElementById("road");
+let bg = document.getElementById("bg");
+let moon = document.getElementById("moon");
+let mountain = document.getElementById("mountain");
+let road = document.getElementById("road");
 
-// window.addEventListener('scroll', function(){
-//     var value = window.scrollY;
+window.addEventListener('scroll', function(){
+    var value = window.scrollY;
 
-//     bg.style.top = value * 0.5 + 'px';
-//     moon.style.left = -value * 0.5 + 'px';
-//     mountain.style.top = -value * 0.15 + 'px';
-//     road.style.top = value * 0.15 + 'px';
-//     text.style.top = value * 1 + 'px';
-// });
+    bg.style.top = value * 0.5 + 'px';
+    moon.style.left = -value * 0.5 + 'px';
+    mountain.style.top = -value * 0.15 + 'px';
+    road.style.top = value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+});
